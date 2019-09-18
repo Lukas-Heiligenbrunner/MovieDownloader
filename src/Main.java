2d4414e5f4bda00aa9a118c64bed81b2f251b9bb
Lukas-Heiligenbrunner <Lukas.Heiligenbrunner@gmail.com>
2019-09-18 11:48:46 +0200

    added dependencies

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        new HDStreamsSeries().capturelinks("https://hd-streams.org/series/family-guy/season/10");
+        new HDStreamsSeries().capturelinks("https://hd-streams.org/series/how-i-met-your-mother/season/5");
         //new HDStreamsMovieCapture().captureMovieLink();
         //new bs().capturelinks("https://bs.to/serie/The-Big-Bang-Theory/5/de");
    }
